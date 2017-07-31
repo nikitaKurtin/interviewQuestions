ae2cc18adc07f169dbfa5825df87353ca0d2a8ea
bubu <nikita.kurtin@gmail.com>
2017-07-31 23:45:13 +0300

    Add solution 3 for finding missing numbers

--- answerExamples.java
+++ answerExamples.java
@@ -57,7 +57,27 @@ public class answerExamples {
     }
 
     //Solution 3 - Assume 2 numbers are missing and Max value is unknown
-    //TODO
+    public static int [] findMissing3(int [] arr){
+        int sum = 0, max = 0, misSum;
+        int [] misNums = {};//empty by default
+        for(int n : arr){
+            sum += n;
+            if(n > max) max = n;
+        }
+        misSum = (max*(max+1)/2) - sum;//Sum of two missing numbers
+        for(int n = Math.min(misSum, max-1); n > 1; n--){
+            if(!contains(n, arr))misNums = new int[]{n, misSum-n};
+
+        }
+        return misNums;
+    }
+    //Utility method - reused in Solution 3 & 4 in finding missing numbers question
+    private static boolean contains(int num, int [] arr){
+        for(int n : arr){
+            if(n == num)return true;
+        }
+        return false;
+    }
 
     //Solution 4 - Assume unknown amount of numbers are missing and Max value is unknown
     //TODO
