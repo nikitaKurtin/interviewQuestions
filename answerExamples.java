74be7e5dd07f7146dcf987f3d23042dc920a2ad0
bubu <nikita.kurtin@gmail.com>
2017-08-01 12:12:41 +0300

    Finish answers in java

--- answerExamples.java
+++ answerExamples.java
@@ -79,31 +79,123 @@ public class answerExamples {
         return false;
     }
 
+
     //Solution 4 - Assume unknown amount of numbers are missing and Max value is unknown
-    //TODO
+    public static ArrayList<Integer> findMissing4(ArrayList<Integer> arr){
+        int sum = 0, count = 0, max = 0, misSum;
+        ArrayList <Integer> misNums=new ArrayList();
+        for(int n : arr){
+            count++;
+            sum += n;
+            if(n > max) max = n;
+        }
+        misSum = (max*(max+1)/2) - sum;//Sum of all missing numbers
+        count = max - count;//How many numbers are missing
+        for(int n = Math.min(misSum, max-1); n > 1; n--){
+            if(arr.indexOf(n) < 0){
+                misNums.add(n);
+                misSum -= n;
+                if(--count == 1){//only one left
+                    misNums.add(misSum);
+                    break;
+                }
+            }
+        }
+        return misNums;
+    }
 
     //Solution 5 - Assume unknown amount of numbers are missing and Max value is unknown (using second array)
-    //TODO
+    public static ArrayList<Integer> findMissing5(ArrayList<Integer> arr){
+        int max = 0;
+        ArrayList<Integer> misNums = new ArrayList();
+        int [] neededNums;
+        for(int n : arr){
+            if(n > max) max = n;
+        }
+        neededNums = new int[max];//zero for any needed num
+        for(int n : arr){//iterate again
+            neededNums[n == max ? 0 : n]++;//add one - used as index in second array (convert max to zero)
+        }
+        for(int i=neededNums.length-1; i>0; i--){
+            if(neededNums[i] < 1)misNums.add(i);//if value is zero, than index is a missing number
+        }
+        return misNums;
+    }
 
     //Question: find biggest contiguous sub array
-    //TODO
+    public static ArrayList <Integer> biggestSubArr(ArrayList <Integer> arr){
+        Integer max = Integer.MIN_VALUE, sum = 0;
+        ArrayList <Integer> test = new ArrayList(), found = new ArrayList();
+        for(int n : arr){
+            sum += n;
+            test.add(n);
+            if(max < sum ){
+                max = sum;
+                found = new ArrayList(test);//clone existing test values to found
+            }
+            if(sum < 0){
+                sum = 0;
+                test.clear();
+            }
+        }
+        return found;
+    }
 
     //Question: Shuffle an array
-    //TODO
+    public static int [] shuffle(int [] arr){
+        int [] shuffle = Arrays.copyOf(arr, arr.length);
+        for(int i=shuffle.length-1; i >= 0; i--){
+            int r = (int)(Math.random() * i);
+            int t = shuffle[r];
+            shuffle[r] = shuffle[i];
+            shuffle[i] = t;
+        }
+        return shuffle;
+    }
 
     //Question: count all digits in a number
 
     //Solution 1 - dynamic type conversion String -> Number -> String
-    //TODO
+    public static int [] countDigits1(long n){
+        char [] num = (""+n).toCharArray();//stringify the number
+        int [] digits = new int [10];//ten zeros
+        for(char d : num){
+            digits[Integer.parseInt(""+d)]++;
+        }
+        return digits;
+    }
 
     //Solution 2 - without type conversion
-    //TODO
+    public static int [] countDigits2(long num){
+        int [] digits = new int [10];//ten zeros
+        for(int d; num > 0 && 0 <= (d = (int)num%10); ){
+            digits[d]++;
+            num = num / 10;
+        }
+        return digits;
+    }
 
     //Question: Find duplicated int in array with values between 0...n (unordered). Cannot use additional memory.
 
     //Solution 1 - Using sorting O(n^2).
-    //TODO
+    public static int findDup1(int [] arr){
+        Arrays.sort(arr);//Bottle neck of this solution
+        for(int i=arr.length-1; i>=0; i--){
+            int t = arr[i];
+            if(t == arr[i-1])return t;
+        }
+        return 0;
+    }
 
     //Solution 2 - Without sorting O(n).
-    //TODO
+    public static int findDup2(int [] arr){
+        for(int n : arr){
+            int t = n<0 ? n*-1 : n;//if negative turn to positive
+            arr[t] *= -1;
+        }
+        for(int n : arr){
+            if(n > 0)return n;
+        }
+        return 0;
+    }
 }
